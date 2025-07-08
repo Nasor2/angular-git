b2d6c041e08fabc5e6a43edaeef1aebe9802b791
Nasor2 <penaortegasamuel@gmail.com>
2025-07-08 16:06:39 -0500

    style: minimalist styled monochromatic palette added

--- src/app/features/posts/pages/post-view/post-view.component.ts
+++ src/app/features/posts/pages/post-view/post-view.component.ts
@@ -4,6 +4,8 @@ import { Post } from '../../../../core/interfaces/post.interface';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PostsService } from '../../posts.service';
 import { of, switchMap } from 'rxjs';
+import { User } from '../../../../core/interfaces/user.interface';
+import { UsersService } from '../../../users/user.service';
 
 @Component({
   standalone: true,
@@ -13,13 +15,14 @@ import { of, switchMap } from 'rxjs';
   styleUrls: ['./post-view.component.scss']
 })
 export class PostViewComponent {
-post: Post | null = null;
-
+  post: Post | null = null;
+  userName = 'Desconocido';
   constructor(
     private route: ActivatedRoute,
     private postsService: PostsService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private usersService: UsersService
+  ) { }
 
   ngOnInit(): void {
     this.route.paramMap
@@ -31,10 +34,19 @@ post: Post | null = null;
         })
       )
       .subscribe({
-        next: post => this.post = post,
+        next: post => {
+          this.post = post;
+          if (post) {
+            this.usersService.getUsers().subscribe(users => {
+              const user = users.find(u => u.id === post.userId);
+              this.userName = user ? user.name : 'Desconocido';
+            });
+          }
+        },
         error: () => this.post = null
       });
   }
+
 
   goBack() {
     this.router.navigate(['/']);
